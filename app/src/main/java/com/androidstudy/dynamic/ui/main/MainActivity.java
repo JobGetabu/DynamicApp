f07a2e7478f0ef6a716ff00964eb4ae6c914d18c
Juma Allan <allanjuma@gmail.com>
2018-04-02 17:50:50 +0300

    Refactored Main Activity

--- app/src/main/java/com/androidstudy/dynamic/ui/main/MainActivity.java
+++ app/src/main/java/com/androidstudy/dynamic/ui/main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.androidstudy.dynamic;
+package com.androidstudy.dynamic.ui.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.androidstudy.dynamic.R;
 
 public class MainActivity extends AppCompatActivity {
 
